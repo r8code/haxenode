d9851f49ff1c991a32550641853dd460f80c57a8
Matthijs Kamstra aka [mck] <matthijs@fonkmobile.nl>
2015-09-28 23:31:36 +0200

    installation haxe update

---
+++ 99test/code/src/Main.hx
@@ -0,0 +1,19 @@
+package ;
+
+import js.Node;
+
+/**
+ * @author Matthijs Kamstra aka [mck]
+ */
+class Main
+{
+	function new()
+	{
+		
+	}
+
+	static public function main()
+	{
+		var main = new Main();
+	}
+}
